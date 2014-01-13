bf87c5eab9776eda0ac454ad8b120d3969b17f3f
David C Danko <dcdanko@gmail.com>
2014-01-13 13:22:21 -0500

    sdf fixer

---
+++ scripts/sdf_fixer.py
@@ -0,0 +1,22 @@
+import sys
+
+def fix( filename):
+
+	f = file(filename, 'r')
+	fixed = file('fixed_'+filename, 'w')
+	for line in f:
+		if "$$$$" in line:
+			end = line.split("$$$$")[1]
+			fixed.write("$$$$\n"+end)
+		else:
+			fixed.write(line)
+	f.close()
+	fixed.close()
+
+if __name__ == "__main__":
+	for filename in sys.argv[1:]:
+		fix( filename)
+
+
+
+
