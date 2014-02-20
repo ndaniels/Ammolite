4967cbcf75d743bdee62c2f7ff4b4d775026864e
David C Danko <dcdanko@gmail.com>
2014-02-20 16:37:32 -0500

    silenced fmcs logger (really)

--- src/edu/mit/csail/ammolite/Logger.java
+++ src/edu/mit/csail/ammolite/Logger.java
@@ -3,7 +3,7 @@ package edu.mit.csail.ammolite;
 public class Logger {
 
 	private static int verbosity = 1;// Normal level output, 0 is quiet, 2 slightly verbose, etc
-	private static boolean showDebug = true;
+	private static boolean showDebug = false;
 	private static boolean experimenting = true;
 	public static void bugOut(boolean _showDebug){
 		showDebug = _showDebug;
