f05d2f482e5009acb3fdb42467ee9f7a886cdca0
David C Danko <dcdanko@gmail.com>
2014-07-10 15:03:39 -0400

    small changes to script

--- scripts/basicSearchTest.py
+++ scripts/basicSearchTest.py
@@ -4,7 +4,9 @@ import sys
 def doSearch(database, queries, coarse, fine, logname):
 	command = "./Ammolite test-search -d {} -q {} -t {} -p {} > {}"
 	command = command.format(database,queries, fine, coarse, logname)
+	print(command)
 	call(command,shell=True)
+	print("Done...")
 
 def main(inName, day):
 	if inName[-4:] in [".adb", ".sdf"] :
@@ -15,7 +17,7 @@ def main(inName, day):
 	basicName = "Search_"+day+"_{}_{}.test"
 
 	for fine in [0.7, 0.8, 0.9]:
-		for coarse in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]:
+		for coarse in [0.1, 0.2, 0.3, 0.4, 0.5]:
 			name = basicName.format(fine,coarse)
 			doSearch(database, queries, coarse, fine, name)
 
