a8807055d7a412a5dbe3705ea5832aaadfae505c
David C Danko <dcdanko@gmail.com>
2014-08-24 16:24:19 -0400

    added hit/miss reporting to caching db

--- src/edu/mit/csail/ammolite/database/CachingStructDatabase.java
+++ src/edu/mit/csail/ammolite/database/CachingStructDatabase.java
@@ -7,6 +7,8 @@ import edu.mit.csail.ammolite.utils.SLRUCache;
 public class CachingStructDatabase extends StructDatabase {
 	protected float memoryLoadFactor = 0.2f;
 	SLRUCache<String,IAtomContainer> cache;
+	private int hits = 0;;
+	private int misses = 0;
 	
 	public CachingStructDatabase(IDatabaseCoreData core){
 		super(core);
@@ -15,13 +17,18 @@ public class CachingStructDatabase extends StructDatabase {
 	
 	@Override
 	public IAtomContainer getMolecule(String pubchemID){
+		if(System.currentTimeMillis() % 1000*60*60 == 0){
+			System.out.println("Hits: "+hits+" Misses: "+misses);
+		}
 		if( cache.containsKey( pubchemID)){
+			hits++;
 			return cache.get(pubchemID);
 		}
-		
+		misses++;
 		IAtomContainer mol = super.getMolecule(pubchemID);
 		cache.put(pubchemID, mol);
 		return mol;
 	}
+	
 
 }
