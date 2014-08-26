7fe04a824da8793e50e911ae5cbce86786a74b9a
David C Danko <dcdanko@gmail.com>
2014-08-26 09:18:37 -0400

    Added caching for organized databases

--- src/edu/mit/csail/ammolite/database/CachingStructDatabase.java
+++ src/edu/mit/csail/ammolite/database/CachingStructDatabase.java
@@ -1,19 +1,28 @@
 package edu.mit.csail.ammolite.database;
 
+import java.util.List;
+
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 import edu.mit.csail.ammolite.utils.PubchemID;
 import edu.mit.csail.ammolite.utils.SLRUCache;
+import edu.mit.csail.ammolite.utils.StructID;
 
 public class CachingStructDatabase extends StructDatabase {
 	protected float memoryLoadFactor = 0.2f;
 	SLRUCache<PubchemID,IAtomContainer> cache;
-	private int hits = 0;;
+	SLRUCache<StructID,List<IAtomContainer>> structCache;
+	private int hits = 0;
 	private int misses = 0;
+	private int sHits = 0;
+	private int sMisses = 0;
 	
 	public CachingStructDatabase(IDatabaseCoreData core){
 		super(core);
 		cache = new SLRUCache<PubchemID,IAtomContainer>(memoryLoadFactor);
+		if( this.isOrganized()){
+			structCache = new SLRUCache<StructID,List<IAtomContainer>>(memoryLoadFactor);
+		}
 	}
 	
 	@Override
@@ -29,6 +38,26 @@ public class CachingStructDatabase extends StructDatabase {
 		IAtomContainer mol = super.getMolecule(pubchemID);
 		cache.put(pubchemID, mol);
 		return mol;
+	}
+	
+	@Override
+	public List<IAtomContainer> getMatchingMolecules(StructID sID){
+		if(System.currentTimeMillis() % 1000*60*60 == 0){
+			System.out.println("Structure_Hits: "+sHits+" Structure_Misses: "+sMisses);
+		}
+		
+		if(!isOrganized()){
+			super.getMatchingMolecules(sID);
+		}
+		
+		if(structCache.containsKey(sID)){
+			sHits++;
+			return structCache.get(sID);
+		}
+		sMisses++;
+		List<IAtomContainer> l = super.getMatchingMolecules(sID);
+		structCache.put(sID, l);
+		return l;
 	}
 	
 
