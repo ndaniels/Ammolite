dc0f9ba85b7db32b42e273082b48fa4e5cb00654
dcdanko <dcdanko@gmail.com>
2014-12-04 20:19:06 -0500

    no blocking threads

--- src/edu/mit/csail/ammolite/tests/Ammolite_QuerywiseParallel_3.java
+++ src/edu/mit/csail/ammolite/tests/Ammolite_QuerywiseParallel_3.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -32,7 +33,7 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
     private static final String NAME = "Ammolite_QuerywiseParallel_Built_In_Timeout";
     private static final int COARSE_QUEUE_SIZE = 250;
     private static final int FINE_QUEUE_SIZE = 250;
-    private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
+    private static final int NUM_CONSUMERS = 2;
     private long TIMEOUT;
     private static final long DEF_TIMEOUT = 2000;
     private static final ExecutorService service = ParallelUtils.buildNewExecutorService();
@@ -87,12 +88,12 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
         CommandLineProgressBar bar = new CommandLineProgressBar(MolUtils.getPubID(query).toString(), coarseHits.size()*12);
         SearchResult result = new SearchResult(query, getName());
         result.start();
-        BlockingQueue<SMSD> queue = new ArrayBlockingQueue<SMSD>(FINE_QUEUE_SIZE,false);
+        BlockingQueue<Future<SMSD>> queue = new ArrayBlockingQueue<Future<SMSD>>(FINE_QUEUE_SIZE,false);
 
         Thread producer = new Thread( new FineProducer(query, coarseHits, db, queue));
         producer.start();
-        List<Thread> consumers = new ArrayList<Thread>(NUM_THREADS);
-        for(int i=0; i<NUM_THREADS; i++){
+        List<Thread> consumers = new ArrayList<Thread>(NUM_CONSUMERS);
+        for(int i=0; i<NUM_CONSUMERS; i++){
             Thread t = new Thread( new FineConsumer(query, queue, result, bar, thresh));
             consumers.add(t);
             t.start();
@@ -124,12 +125,12 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
         CommandLineProgressBar bar = new CommandLineProgressBar(MolUtils.getPubID(query).toString() + "_COARSE", numReps);
         SearchResult coarseResult = new SearchResult(cQuery, getName() + "_COARSE");
         coarseResult.start();
-        BlockingQueue<SMSD> queue = new ArrayBlockingQueue<SMSD>(COARSE_QUEUE_SIZE,false);
+        BlockingQueue<Future<SMSD>> queue = new ArrayBlockingQueue<Future<SMSD>>(COARSE_QUEUE_SIZE,false);
         Collection<StructID> hits = Collections.synchronizedCollection(new HashSet<StructID>());
         Thread producer = new Thread( new CoarseProducer(cQuery, sTargets, queue));
         producer.start();
-        List<Thread> consumers = new ArrayList<Thread>(NUM_THREADS);
-        for(int i=0; i<NUM_THREADS; i++){
+        List<Thread> consumers = new ArrayList<Thread>(NUM_CONSUMERS);
+        for(int i=0; i<NUM_CONSUMERS; i++){
             Thread t = new Thread( new CoarseConsumer(cQuery, queue, coarseResult, hits, bar, thresh));
             consumers.add(t);
             t.start();
@@ -162,11 +163,11 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
     }
     
     private class CoarseProducer implements Runnable {
-        BlockingQueue< SMSD> queue;
+        BlockingQueue<Future<SMSD>> queue;
         Iterator<MolStruct> targets;
         MolStruct query;
         
-        public CoarseProducer(MolStruct query, Iterator<MolStruct> sTargets, BlockingQueue< SMSD> queue){
+        public CoarseProducer(MolStruct query, Iterator<MolStruct> sTargets, BlockingQueue<Future<SMSD>> queue){
               this.targets = sTargets;
             this.queue = queue;
             this.query = query;
@@ -174,11 +175,17 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
 
         @Override
         public void run() {
-            MolStruct target;
              while(targets.hasNext()){
-                target = targets.next();
+                final MolStruct  target = targets.next();
+                Future<SMSD> future = service.submit(new Callable<SMSD>(){
+                    public SMSD call(){
+                        SMSD smsd = new SMSD(query, target);
+                        smsd.calculate();
+                        return smsd;
+                    }
+                });
                 try {
-                    queue.offer(new SMSD(query, target), 1, TimeUnit.MINUTES);
+                    queue.offer(future, 1, TimeUnit.MINUTES);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } 
@@ -188,12 +195,12 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
     }
     
     private class FineProducer implements Runnable {
-        BlockingQueue< SMSD> queue;
+        BlockingQueue<Future<SMSD>> queue;
         Collection<StructID> hits;
         IStructDatabase db;
         IAtomContainer query;
         
-        public FineProducer(IAtomContainer query, Collection<StructID> hits, IStructDatabase db, BlockingQueue< SMSD> queue){
+        public FineProducer(IAtomContainer query, Collection<StructID> hits, IStructDatabase db, BlockingQueue<Future<SMSD>> queue){
             this.hits = hits;
             this.queue = queue;
             this.db = db;
@@ -204,8 +211,16 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
         public void run() {
             for(StructID hit: hits){
                 for(IAtomContainer target: db.getMatchingMolecules(hit)){
+                    final IAtomContainer  fTarget = target;
+                    Future<SMSD> future = service.submit(new Callable<SMSD>(){
+                        public SMSD call(){
+                            SMSD smsd = new SMSD(query, fTarget);
+                            smsd.calculate();
+                            return smsd;
+                        }
+                    });
                     try {
-                        queue.offer(new SMSD(query, target), 1, TimeUnit.MINUTES);
+                        queue.offer(future, 1, TimeUnit.MINUTES);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -216,14 +231,14 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
     }
     
     private class CoarseConsumer implements Runnable {
-        BlockingQueue<SMSD> queue;
+        BlockingQueue<Future<SMSD>> queue;
         MolStruct query;
         SearchResult result;
         Collection<StructID> hits;
         CommandLineProgressBar bar;
         double threshold;
         
-        public CoarseConsumer(MolStruct cQuery, BlockingQueue<SMSD> queue, SearchResult result, Collection<StructID> hits, CommandLineProgressBar bar, double threshold){
+        public CoarseConsumer(MolStruct cQuery, BlockingQueue<Future<SMSD>> queue, SearchResult result, Collection<StructID> hits, CommandLineProgressBar bar, double threshold){
             this.query = cQuery;
             this.queue = queue;
             this.result = result;
@@ -237,18 +252,12 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
             while(!Thread.currentThread().isInterrupted()){
                 try {
                     
-                    final SMSD smsd = queue.poll(500, TimeUnit.MILLISECONDS);
-                    if(smsd != null){
-                        
-                        Future future = service.submit(new Runnable(){
-                            public void run(){
-                                smsd.calculate();
-                            }
-                        });
-                        
+                    Future<SMSD> future = queue.poll(500, TimeUnit.MILLISECONDS);
+                    if(future != null){
+                        SMSD smsd = null;
                         boolean timedOut = false;
                         try {
-                            future.get(TIMEOUT, TimeUnit.MILLISECONDS);
+                            smsd = future.get(TIMEOUT, TimeUnit.MILLISECONDS);
                         } catch (TimeoutException e) {
                             timedOut = true;
                         } catch (ExecutionException e){
@@ -283,13 +292,13 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
     }
     
     private class FineConsumer implements Runnable {
-        BlockingQueue<SMSD> queue;
+        BlockingQueue<Future<SMSD>> queue;
         IAtomContainer query;
         SearchResult result;
         CommandLineProgressBar bar;
         double threshold;
         
-        public FineConsumer(IAtomContainer query, BlockingQueue<SMSD> queue, SearchResult result, CommandLineProgressBar bar, double threshold){
+        public FineConsumer(IAtomContainer query, BlockingQueue<Future<SMSD>> queue, SearchResult result, CommandLineProgressBar bar, double threshold){
             this.query = query;
             this.queue = queue;
             this.result = result;
@@ -301,18 +310,13 @@ public class Ammolite_QuerywiseParallel_3 implements Tester {
         public void run() {
             while(!Thread.currentThread().isInterrupted()){
                 try {
-                    final SMSD smsd = queue.poll(500, TimeUnit.MILLISECONDS);
-                    if(smsd != null){
-                        
-                        Future future = service.submit(new Runnable(){
-                            public void run(){
-                                smsd.calculate();
-                            }
-                        });
+                    Future<SMSD> future = queue.poll(500, TimeUnit.MILLISECONDS);
+                    if(future != null){
+                        SMSD smsd = null;
                         
                         boolean timedOut = false;
                         try {
-                            future.get(TIMEOUT, TimeUnit.MILLISECONDS);
+                            smsd = future.get(TIMEOUT, TimeUnit.MILLISECONDS);
                         } catch (TimeoutException e) {
                             timedOut = true;
                         } catch (ExecutionException e){
