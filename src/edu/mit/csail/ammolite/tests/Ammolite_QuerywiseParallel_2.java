42319a7279f99c2ac5785dbc8d66c565a26ec392
dcdanko <dcdanko@gmail.com>
2015-03-12 10:57:35 -0400

    larger fine queue size

--- src/edu/mit/csail/ammolite/tests/Ammolite_QuerywiseParallel_2.java
+++ src/edu/mit/csail/ammolite/tests/Ammolite_QuerywiseParallel_2.java
@@ -35,7 +35,7 @@ import edu.mit.csail.ammolite.utils.StructID;
 public class Ammolite_QuerywiseParallel_2 implements Tester {
     private static final String NAME = "Ammolite_Tanimoto";
     private static final int COARSE_QUEUE_SIZE = 1000;
-    private static final int FINE_QUEUE_SIZE = 1000;
+    private static final int FINE_QUEUE_SIZE = 50000;
     private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors()/2;
         
     public Ammolite_QuerywiseParallel_2() {}
