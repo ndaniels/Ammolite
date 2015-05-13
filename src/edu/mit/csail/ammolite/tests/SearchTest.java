b179bd00be7643e66228caddcda37df120550460
dcdanko <dcdanko@gmail.com>
2015-05-13 08:48:31 -0400

    some clean up

--- src/edu/mit/csail/ammolite/tests/SearchTest.java
+++ src/edu/mit/csail/ammolite/tests/SearchTest.java
@@ -73,22 +73,12 @@ public class SearchTest {
     		if( testSMSD){
     			Tester tester = new SMSD_Basic();
     			runTest(tester, stream, queries, db, targets, sTargets,  fine, coarse);
-    		}
-    		
-    		if( testFMCS){
-                Tester tester = new Ammolite_QuerywiseParallel_FMCS();
-                runTest(tester, stream, queries, db, targets, sTargets,  fine, coarse);
-            }
-    		
+    		}	
     		if( testAmmSMSD){
     			Tester tester = new SMSD_HighPass();
     			runTest(tester, stream, queries, db, targets, sTargets,  fine, coarse);
     		} 
     		
-    		if( testAmmPar){
-    		    Tester tester = new Ammolite_Fine_Only();
-                runTest(tester, stream, queries, db, targets, sTargets,  fine, coarse);
-    		}
     
     		stream.close();
 		}
